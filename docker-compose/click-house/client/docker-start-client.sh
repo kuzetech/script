8a82fe8aeb7320a1fb4035bf838b267030ebc4d8
黄少威 <huangshaowei@xmfunny.com>
2021-07-26 18:41:49 +0800

    no message

--- docker-compose/click-house/client/docker-start-client.sh
+++ docker-compose/click-house/client/docker-start-client.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 
-docker run -it --rm --link clickhouse:clickhouse-server --net cknet yandex/clickhouse-client --host clickhouse-server
+HOST_NAME=$1
+
+docker run -it --rm --link ${HOST_NAME}:${HOST_NAME} --net cknet yandex/clickhouse-client --host ${HOST_NAME} --port 9000
