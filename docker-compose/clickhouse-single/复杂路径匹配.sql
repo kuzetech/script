5c72823deac82141d493b41c4fe901a0ecdf5a42
黄少威 <huangshaowei@xmfunny.com>
2021-08-31 14:57:34 +0800

    no message

--- docker-compose/clickhouse-single/复杂路径匹配.sql
+++ docker-compose/clickhouse-single/复杂路径匹配.sql
@@ -12,58 +12,54 @@ create table test_path (
 
 insert into test_path values
 (1, 'login', '2020-01-01', '2020-01-01 01:00:00'),
-(1, 'login', '2020-01-01', '2020-01-01 02:00:00'),
-(1, 'login', '2020-01-01', '2020-01-01 03:00:00'),
-(2, 'login', '2020-01-01', '2020-01-01 01:00:00'),
-(2, 'login', '2020-01-01', '2020-01-01 02:00:00'),
-(2, 'login', '2020-01-01', '2020-01-01 03:00:00');
+(1, 'view', '2020-01-01', '2020-01-01 05:00:00'),
+(1, 'buy', '2020-01-01', '2020-01-01 09:00:00'),
+(1, 'buy', '2020-01-01', '2020-01-01 10:00:00'),
+(1, 'view', '2020-01-01', '2020-01-01 12:00:00'),
+(1, 'buy', '2020-01-01', '2020-01-01 14:00:00');
 
-insert into test_path values
-(3, 'login', '2020-01-01', '2020-01-01 01:01:00'),
-(3, 'login', '2020-01-01', '2020-01-01 01:02:00'),
-(3, 'login', '2020-01-01', '2020-01-01 01:03:00');
 
-select
-    result_chain,
+select 
+    result_chain, 
     uniqCombined(uid) as user_count
 from(
     with
-        toUInt32(maxIf(ts_date_time, event_type = 'buy')) as end_event_max,
+        toUInt32(maxIf(ts_date_time, event_type = 'buy')) as buy_max_time,
+        -- 将用户行为整理成 <时间，事件名>元组，并排序
         arrayCompact(arraySort(
             x -> x.1,
             arrayFilter(
-                x -> x.1 <= end_event_max,
-                groupArray((toUInt32(ts_date_time), (event_type)))
+                x -> x.1 <= buy_max_time,
+                groupArray((toUInt32(ts_date_time), event_type))
             )
         )) as sorted_events,
+        -- 获取原始行为链的下标数组
         arrayEnumerate(sorted_events) as event_idxs,
+        -- 找出链条分界点，事件为 buy 或者 事件间隔超过 600 秒
         arrayFilter(
-            (x, y, z) -> z.1 <= end_event_max and z.2.1 = 'buy' or y > 600),
-            event_idxs,
-            arrayDifference(sorted_events.1),
-            sorted_events
+            (x, y, z) -> z.1 <= buy_max_time and (z.2 = 'buy' or y > 600),
+            event_idxs, -- 作为下标x
+            arrayDifference(sorted_events.1), -- 作为时间间隔y
+            sorted_events -- 作为事件z
         ) as gap_idxs,
+        -- 利用 arrayMap 和 has 函数获取下标数组的掩码（0和1组成），用于最终切分，1表示分界点
+        -- 配合下面的 arraySplit 使用所以需要+1
         arrayMap( x -> x+1, gap_idxs) as gap_idxs_,
         arrayMap( x -> if(has(gap_idxs_, x), 1, 0), event_idxs) as gap_masks,
+        -- 将行为链按分界点切分成单次访问的行为链，函数会将分界点作为新链的起点，所以前面要将分界点+1
         arraySplit((x, y) -> y, sorted_events, gap_masks) as split_events
     select
-        uid,
+        uid, buy_max_time, sorted_events, event_idxs, gap_idxs_, gap_masks, split_events,
         arrayJoin(split_events) as event_chain_,
         arrayCompact(event_chain_.2) as event_chain,
-        hasAll(event_chain, [('login', 'view')]) as has_midway_hit,
-        arrayStringConcat(arrayMap(
-            x -> concat(x.1, '#', x.2),
-            event_chain
-        ), ' -> ') as result_chain
-    from(
-        select ts_date, ts_date_time, event_type, uid
-        from test_path
-        where ts_date = '2020-01-01'
-    )
+        -- 也可以使用 hasAny 函数筛选途经点
+        hasAll(event_chain, ['login', 'view']) as has_midway_hit,
+        arrayStringConcat(arrayMap( x -> x, event_chain), ' -> ') as result_chain
+    from test_path
     group by uid
-    having length(event_chain) > 1
-)
-where event_chain[length(event_chain)].1 = 'buy' and has_midway_hit = 1
-group by result_chain
-order by user_countdesc
-limit 20;
+    having length(event_chain)>1
+) 
+where event_chain[length(event_chain)]='buy' and has_midway_hit=1 
+group by result_chain 
+order by user_count 
+desc limit 20
