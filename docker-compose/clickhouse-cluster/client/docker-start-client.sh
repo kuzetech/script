c3365399baddb083405b5845da58789226a2eac3
黄少威 <huangshaowei@xmfunny.com>
2021-09-02 11:38:55 +0800

    no message

--- docker-compose/clickhouse-cluster/client/docker-start-client.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-HOST_NAME=$1
-
-docker run -it --rm --link ${HOST_NAME}:${HOST_NAME} --net cknet yandex/clickhouse-client --host ${HOST_NAME} --port 9000
-
-#docker run -it --rm --network host yandex/clickhouse-client --host localhost --port 9000
--- docker-compose/clickhouse-sinker/client.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-docker run -it --rm --link clickhouse:clickhouse --net clickhouse-sinker_default yandex/clickhouse-client --host clickhouse --port 9000
