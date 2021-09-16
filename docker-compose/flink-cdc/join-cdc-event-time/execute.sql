67d50e67ce4d56891a9caeedb04696978e14002b
黄少威 <huangshaowei@xmfunny.com>
2021-09-16 10:56:23 +0800

    no message

--- docker-compose/flink-cdc/join-cdc-event-time/execute.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    o.order_id, 
-    o.order_date, 
-    o.product_id, 
-    p.name as product_name, 
-    p.description as product_description
-FROM orders AS o
-INNER JOIN products FOR SYSTEM_TIME AS OF o.order_date AS p 
-ON o.product_id = p.id;
--- docker-compose/flink-cdc/join-kafka/execute.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    o.order_id, 
-    o.order_date, 
-    o.product_id, 
-    p.name as product_name, 
-    p.description as product_description
-FROM orders AS o
-INNER JOIN product FOR SYSTEM_TIME AS OF o.proctime AS p 
-ON o.product_id = p.id;
