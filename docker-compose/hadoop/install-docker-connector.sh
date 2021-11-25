ea94ad867d1ae521c7ea2be87f203cf37fbd6206
黄少威 <huangshaowei@xmfunny.com>
2021-11-25 14:21:13 +0800

    no message

--- docker-compose/hadoop/install-docker-connector.sh
+++
@@ -1,9 +0,0 @@
-#https://www.haoyizebo.com/posts/fd0b9bd8/
-
-brew install wenjunxiao/brew/docker-connector
-
-docker network ls --filter driver=bridge --format "{{.ID}}" | xargs docker network inspect --format "route {{range .IPAM.Config}}{{.Subnet}}{{end}}" >> /usr/local/etc/docker-connector.conf
-
-sudo brew services start docker-connector
-
-docker run -it -d --restart always --net host --cap-add NET_ADMIN --name connector wenjunxiao/mac-docker-connector
