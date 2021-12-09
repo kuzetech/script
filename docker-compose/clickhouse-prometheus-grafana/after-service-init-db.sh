3b975d8155b4c9149fbc599417837db8a669b87a
黄少威 <huangshaowei@xmfunny.com>
2021-12-09 16:50:41 +0800

    no message

---
+++ docker-compose/clickhouse-prometheus-grafana/after-service-init-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ex
+
+clickhouse-client -n <<-EOSQL
+    CREATE TABLE IF NOT EXISTS system.query_log_all ON CLUSTER c1s1r as system.query_log
+    ENGINE = Distributed(c1s1r, system, query_log);
+EOSQL
+
+docker exec clickhouse clickhouse-client -q 'CREATE TABLE IF NOT EXISTS system.query_log_all ON CLUSTER c1s1r as system.query_log ENGINE = Distributed(c1s1r, system, query_log);'
