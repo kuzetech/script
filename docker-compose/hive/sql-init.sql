cb9db8922295281e7069e80378e46576f2907f5e
黄少威 <huangshaowei@xmfunny.com>
2021-12-30 11:04:54 +0800

    no message

--- docker-compose/hive/sql-init.sql
+++ docker-compose/hive/sql-init.sql
@@ -7,11 +7,13 @@ WITH (
 
 USE CATALOG MyCatalog;
 
-CREATE TABLE IF NOT EXISTS mykafka (name String, age Int) WITH (
+DROP TABLE IF EXISTS mykafka;
+CREATE TABLE mykafka (name String, age Int, sex String, id Int) WITH (
   'connector' = 'kafka',
   'topic' = 'test',
   'properties.bootstrap.servers' = 'localhost:9092',
   'properties.group.id' = 'testGroup',
   'scan.startup.mode' = 'earliest-offset',
+  'csv.ignore-parse-errors' = 'true',
   'format' = 'csv'
 );
