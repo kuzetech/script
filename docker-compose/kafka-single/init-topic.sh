c134e608d2f9d291983ed9c4a43407ae91ec17ec
黄少威 <huangshaowei@xmfunny.com>
2022-02-09 14:20:53 +0800

    no message

--- docker-compose/kafka-single/init-topic.sh
+++ docker-compose/kafka-single/init-topic.sh
@@ -1,18 +1,31 @@
+#!/bin/bash
+
+set +x
+
 are_brokers_available() {
-    expected_num_brokers=2 # replace this with a proper value
-    num_brokers=1
-    return [ $num_brokers -gt $expected_num_brokers ]
+    expected_num_brokers=1 # replace this with a proper value
+    brokers_str="$(zookeeper-shell.sh zookeeper:2181 ls /brokers/ids 2>/dev/null | grep "\[")"
+    echo $brokers_str
+    array=(${brokers_str//,/ })
+    num_brokers=${#array[*]}
+    echo $num_brokers
+    if [ $num_brokers -ge $expected_num_brokers ]
+    then
+        return 1
+    else
+        return 0
+    fi
 }
 
-while ! are_brokers_available; do
-    echo $are_brokers_available
+while are_brokers_available 
+do
     echo "brokers not available yet"
     sleep 1
 done
 
 echo "Kafka custom init"
 
-/opt/bitnami/kafka/bin/kafka-topics.sh --zookeeper zookeeper:2181 --create  --replication-factor 1 --partitions 1 --topic testovic
+/opt/bitnami/kafka/bin/kafka-topics.sh --zookeeper zookeeper:2181 --create  --replication-factor 1 --partitions 3 --topic test123
 /opt/bitnami/kafka/bin/kafka-topics.sh --zookeeper zookeeper:2181 --list
 
 echo "Kafka custom init done"
