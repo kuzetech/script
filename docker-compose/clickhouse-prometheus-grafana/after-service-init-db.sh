eec498e935aec3803dd596e75ae026af5ac3af72
黄少威 <huangshaowei@xmfunny.com>
2022-05-18 10:58:51 +0800

    no message

--- docker-compose/clickhouse-prometheus-grafana/after-service-init-db.sh
+++ docker-compose/clickhouse-prometheus-grafana/after-service-init-db.sh
@@ -6,4 +6,7 @@ clickhouse-client -n <<-EOSQL
     ENGINE = Distributed(c1s1r, system, query_log);
 EOSQL
 
-# docker exec clickhouse clickhouse-client -q 'CREATE TABLE IF NOT EXISTS system.query_log_all ON CLUSTER c1s1r as system.query_log ENGINE = Distributed(c1s1r, system, query_log);'
+
+# CREATE TABLE IF NOT EXISTS system.query_log_all ON CLUSTER default as system.query_log ENGINE = Distributed(default, system, query_log);
+
+# CREATE TABLE IF NOT EXISTS system.parts_all ON CLUSTER default as system.parts ENGINE = Distributed(default, system, parts);
