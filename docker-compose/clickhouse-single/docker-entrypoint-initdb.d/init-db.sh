2a87d63484ac000dbfa9b0da19016ba3e0bd7816
黄少威 <huangshaowei@xmfunny.com>
2022-06-10 18:59:52 +0800

    no message

--- docker-compose/clickhouse-single/docker-entrypoint-initdb.d/init-db.sh
+++ docker-compose/clickhouse-single/docker-entrypoint-initdb.d/init-db.sh
@@ -4,17 +4,15 @@ set -e
 clickhouse client -n <<-EOSQL
     use default;
 
-    CREATE TABLE event_local
-    (
-        id          UInt32,
-        event        String,
-        time        Int64,
-        dt          Date
-    )
-    ENGINE = MergeTree
-    PARTITION BY (dt, event)
-    ORDER BY (time, id);
+    create table test (
+        uid         String      COMMENT '用户',
+        eventId     String      COMMENT '事件名称',
+        eventTime   Date        COMMENT '事件时间'
+    ) engine = Memory;
 
-    insert into event_local values (1, 'a', 1,'2022-05-03'), (2, 'b', 2, '2022-05-04');
+    insert into test values ('小明', '登录', '2020-01-01'), ('小明', '升级', '2020-01-01'), ('小明', '充值', '2020-01-01');
+    insert into test values ('小明', '登录', '2020-01-02'); 
+    insert into test values ('小明', '升级', '2020-01-03'); 
+    insert into test values ('小明', '充值', '2020-01-04');
 
 EOSQL
