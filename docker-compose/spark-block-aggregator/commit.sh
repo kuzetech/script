25eaaaab25e83cd282675747fc38b8431ee6f8fd
黄少威 <huangshaowei@xmfunny.com>
2022-08-03 18:37:40 +0800

    no message

---
+++ docker-compose/spark-block-aggregator/commit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+# --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.3 \
+
+spark-submit \
+--master spark://master:7077 \
+--executor-memory 1G \
+--total-executor-cores 2 \
+/spark_src/spark-block-aggregator-1.0-SNAPSHOT.jar /spark_src/config-cluster.json
