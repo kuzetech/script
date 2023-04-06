6859a2c31d65b42e209443151e1be01c872e9858
黄少威 <huangshaowei@xmfunny.com>
2023-04-06 16:16:22 +0800

    no message

---
+++ mac/add-docker-router.sh
@@ -0,0 +1,2 @@
+# 执行下面命令将docker所有 bridge 网络都添加到docker-connector路由
+docker network ls --filter driver=bridge --format "{{.ID}}" | xargs docker network inspect --format "route {{range .IPAM.Config}}{{.Subnet}}{{end}}" >> /usr/local/etc/docker-connector.conf
