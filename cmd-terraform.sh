b2aa379cef807fd99a51103713ce10b1297933f0
黄少威 <huangshaowei@xmfunny.com>
2024-09-29 16:18:18 +0800

    no message

--- cmd-terraform.sh
+++ cmd-terraform.sh
@@ -1,9 +1,9 @@
 
 cd platform
 
-./chartprep.sh
-
 terraform init
+
+./chartprep.sh
 
 然后修改 flink job 配置
 
@@ -19,3 +19,4 @@ terraform apply --target=module.main.module.funnydb-mutation-event-processor
 
 initialSavepointPath: >-
   alluxio://alluxio-master.default.svc.cluster.local:19998/stage-api-server/flink/application/funnydb-track-event-process-flow-controller/savepoints/savepoint-a71ce9-85862d6378a0
+  
