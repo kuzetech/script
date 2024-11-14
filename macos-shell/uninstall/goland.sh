5e529da413f97cf8bfcdc582f47acfa27c590857
kuze <370415788@qq.com>
2024-11-15 07:48:06 +0800

    no message

--- macos-shell/uninstall/goland.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-## 第一步先删除 mac 上的 goland 程序
-## 第二步执行该脚本
-
-rm -rf ~/Library/Application Support/JetBrains/GoLand*
-rm -rf ~/Library/Preferences/*goland*
-rm -rf ~/Library/Logs/JetBrains//GoLand*
