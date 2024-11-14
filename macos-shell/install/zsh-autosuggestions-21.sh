6bea126702e1b24d3780508d43c391cc3af822dc
kuze <370415788@qq.com>
2024-11-14 21:44:08 +0800

    no message

---
+++ macos-shell/install/zsh-autosuggestions-21.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+brew install zsh-autosuggestions
+
+echo -e "\n"
+
+echo "source /opt/homebrew/Cellar/zsh-autosuggestions/0.7.0/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
