6ccd366890e62e41aa3f5e0bcd0fabd41932d7fe
huangshaowei <huangshaowei@xmfunny.com>
2025-03-02 20:49:25 +0800

    no message

---
+++ macos-shell/install/homebrew.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+/bin/bash -c "$(curl -fsSL https://gitee.com/ineo6/homebrew-install/raw/master/install.sh)"
--- macos-shell/study/test.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-name=big
-echo $(echo $name
-name=sd
-echo $name)
-
-# 结果是 big sd
