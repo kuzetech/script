a20ff7dd04474f2877233880a7cefea4279b764f
黄少威 <huangshaowei@xmfunny.com>
2026-02-25 16:45:39 +0800

    no message

---
+++ funnydb/clickhouse.sql
@@ -0,0 +1,16 @@
+formatDateTime(fromUnixTimestamp64Milli(`#created_time`), '%Y-%m-%d %H:%i:%s.%f', 'Asia/Shanghai')
+
+SELECT
+    multiIf(
+    `#duration` < 0, '小于0',
+    `#duration` = 0, '等于0',
+    '大于0'
+    ) AS duration_group,
+    count() AS num,
+    sum(count()) OVER () AS total,
+    round(count() * 100 / total, 2) AS ratio
+FROM events
+WHERE `#dt` = '2026-01-14'
+AND `#event` = '#user_online_duration'
+AND `#sdk_type` = 'flink-derive'
+GROUP BY duration_group;
